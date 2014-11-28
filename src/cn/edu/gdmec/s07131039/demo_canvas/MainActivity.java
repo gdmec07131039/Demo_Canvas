4a9022bc8f7d379d92c98cc7b0d4d97f2191050c
gdmec07131043 <853838768@qq.com>
2014-11-28 11:40:54 +0800

    “Demo_Canvas”

---
+++ src/cn/edu/gdmec/s07131039/demo_canvas/MainActivity.java
@@ -0,0 +1,24 @@
+package cn.edu.gdmec.s07131039.demo_canvas;
+
+import cn.edu.gdmec.s07131039.demo_canvas.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyView(this));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
